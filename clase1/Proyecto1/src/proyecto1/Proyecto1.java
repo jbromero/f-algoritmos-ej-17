1375bb1c0681d88a5f4b5d3ddd48d9d3ad47d42c
jbromero <jobenromsal@gmail.com>
2017-10-04 12:06:56 -0500

    Terminar trabajo

--- clase1/Proyecto1/src/proyecto1/Proyecto1.java
+++ clase1/Proyecto1/src/proyecto1/Proyecto1.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto1;
-
+import java.util.Scanner;
 /**
- *
- * @author Salas
+ * @author jbromero3
  */
 public class Proyecto1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado= new Scanner(System.in);
+        int mult=0;
+        int multi=0;
+        
+        System.out.print("Ingrese primer valor: ");
+        mult = teclado.nextInt();
+        System.out.print("Ingrese segundo valor: ");
+        multi = teclado.nextInt();
+        
+        System.out.print("La multiplicacion es : "+mult*multi);
     }
-    
 }
